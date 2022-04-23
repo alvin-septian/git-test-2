ace3b8069d45c7f0dfaad12b78a34d2c3517f521
alvin-septian <alvinseptianakbar@gmail.com>
2022-04-23 14:31:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,12 @@
 <body>
 	<h1>Hello World</h1>
 	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa in, culpa, excepturi modi voluptates quaerat sapiente. Nihil, distinctio, dolor est placeat alias sed voluptate perspiciatis. Delectus nemo, ut accusamus unde?</p>
+	<ul>
+		<li>1</li>
+		<li>2</li>
+		<li>3</li>
+		<li>4</li>
+		<li>5</li>
+	</ul>
 </body>
 </html>
